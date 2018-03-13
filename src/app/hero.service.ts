fe787eeb820c748270357fe29ab7f819264a892c
Florian Leroux <florian.leroux@prismo.io>
2018-03-13 17:24:58 +0100

    Init HttpClient & Error Handling

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+
 import { Hero } from './hero';
 
 /* Services */
@@ -10,16 +13,44 @@ import { MessageService } from './message.service';
 @Injectable()
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes'; // URL to web API
 
-  getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient
+  ) { }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add('HeroService: ' + message);
   }
 
-  getHero(id: number): Observable<Hero> {
+  getHeroes(): Observable<Hero[]> {
+    this.log('fetched heroes');
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        catchError(this.handleError('getHeroes', []))
+      );
+  }
+
+ /*  getHero(id: number): Observable<Hero> {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    return of(heroes.find(hero => hero.id === id));
+  } */
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+
+      return of(result as T);
+    };
   }
 
 }
--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -8,7 +8,7 @@ import { Hero } from './hero';
 export class InMemoryDataService implements InMemoryDbService {
 
   createDb() {
-    const HEROES: Hero[] = [
+    const heroes: Hero[] = [
         { id: 11, name: 'Mr. Nice'},
         { id: 12, name: 'Narco'},
         { id: 13, name: 'Bombasto'},
@@ -20,7 +20,7 @@ export class InMemoryDataService implements InMemoryDbService {
         { id: 19, name: 'Magma'},
         { id: 20, name: 'Tornado'}
     ];
-    return { HEROES };
+    return { heroes };
   }
 
 
