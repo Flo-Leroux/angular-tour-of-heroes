e307054d0b5b4015d2a2c7436f5fab18af93de1d
Florian Leroux <florian.leroux@prismo.io>
2018-03-13 17:31:14 +0100

    HttpClient for getHero by ID

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -26,17 +26,21 @@ export class HeroService {
   }
 
   getHeroes(): Observable<Hero[]> {
-    this.log('fetched heroes');
     return this.http.get<Hero[]>(this.heroesUrl)
       .pipe(
+        tap(heroes => this.log('fetched heroes')),
         catchError(this.handleError('getHeroes', []))
       );
   }
 
- /*  getHero(id: number): Observable<Hero> {
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(heroes.find(hero => hero.id === id));
-  } */
+  getHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
 
   /**
    * Handle Http operation that failed.
